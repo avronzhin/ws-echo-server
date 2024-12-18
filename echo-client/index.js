b9585b9883cd1fc573f101750f60e131dd723ac5
avronzhin <avronzhin@yandex.ru>
2024-12-18 22:31:40 +0300

    refactored

--- echo-client/index.js
+++ echo-client/index.js
@@ -1,63 +1,106 @@
 import WebSocket from "ws";
 import readline from "readline";
-import { on } from "events";
+
+const serverUrl = "ws:/localhost:8080/ws/api/v1";
+var client = null;
+var connecting = false;
+var connected = false;
 
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    prompt: ""
-})
+const consoleInterface = createConsoleInterface();
+printMenuText();
+consoleInterface.prompt();
 
-var serverUrl = "ws:/localhost:8080/ws/api/v1";
+function createConsoleInterface() {
+    const readlineInterface = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: ""
+    })
+    readlineInterface.on("line", onConsoleLine);
+    readlineInterface.on("close", onConsoleClose);
+    return readlineInterface;
+}
 
-var client
-
-rl.prompt();
-
-console.log("Вы в меню, для вывода списка возможных комманд введите help")
-var online = false
-rl.on("line", (line) => {
-    const arg = line.trim()
-    if (!online) {
-        switch (arg) {
-            case "help":
-                console.log("connect - для подключения к серверу")
-                console.log("exit - для закрытия приложения")
-                console.log("help - для вывода возможных комманд")
-                break;
-            case "connect":
-                console.log("Установка соединения с " + serverUrl)
-                client = new WebSocket(serverUrl);
-                client.on("message", (message) => {
-                    console.log("Пришло сообщение от сервера: " + message)
-                })
-
-                client.on("open", () => {
-                    console.log("Установка успешно выполнена. Можете отправлять сообщения. Для отключения введите !q")
-                })
-                online = true
-                break;
-            case "exit":
-                rl.close()
-                break;
-        }
-    } else {
-        if (arg === "!q") {
-            online = false
-            console.log("Остановка соединения с сервером")
-            client.close()
-            client = null
-            console.log("Вы в меню, для вывода списка возможных комманд введите help")
-        } else {
-            console.log("Отправка сообщения: " + arg)
-            client.send(arg)
-        }
+function onConsoleLine(line) {
+    line = line.trim();
+    if (connecting) {
+        console.log("Подождите! Идет настройка соединения");
     }
-    rl.prompt();
-})
 
-rl.on("close", () => {
-    console.log("Завершение работы программы")
+    if (connected) {
+        handleLineInSession(line);
+    } else {
+        handleLineInMenu(line);
+    }
+    consoleInterface.prompt();
+}
+
+function handleLineInMenu(command) {
+    switch (command) {
+        case "help":
+            printHelp();
+            break;
+        case "connect":
+            connect();
+            break;
+        case "exit":
+            consoleInterface.close()
+            break;
+    }
+}
+
+function printHelp() {
+    console.log("connect - для подключения к серверу")
+    console.log("exit - для закрытия приложения")
+    console.log("help - для вывода возможных комманд")
+}
+
+function connect() {
+    console.log("Установка соединения с " + serverUrl);
+    client = new WebSocket(serverUrl);
+    client.on("open", onConnectionOpen);
+    client.on("message", onServerMessage);
+    connecting = true;
+}
+
+function onServerMessage(message) {
+    console.log("Пришло сообщение от сервера: " + message)
+}
+
+function onConnectionOpen() {
+    console.log("Установка успешно выполнена. Можете отправлять сообщения. Для отключения введите !q")
+    connected = true;
+    connecting = false;
+}
+
+function handleLineInSession(line) {
+    if (line === "!q") {
+        disconect();
+    } else {
+        sendMessage(line);
+    }
+}
+
+function disconect() {
+    console.log("Остановка соединения с сервером");
+    client.close();
+    client = null;
+
+    printMenuText();
+    connected = false;
+}
+
+function sendMessage(message) {
+    console.log("Отправка сообщения: " + message);
+    client.send(message);
+}
+
+function onConsoleClose() {
+    console.log("Завершение работы программы");
     process.exit(0);
-})
+}
+
+function printMenuText() {
+    console.log("Вы в меню, для вывода списка возможных комманд введите help")
+}
--- echo-server/index.js
+++ echo-server/index.js
@@ -1,17 +1,24 @@
 import { WebSocketServer } from "ws";
 
-const wss = new WebSocketServer({ port: 8080, path: "/ws/api/v1" });
+startServer();
 
-wss.on("connection", (ws) => {
+function startServer() {
+    const wss = new WebSocketServer({ port: 8080, path: "/ws/api/v1" });
+    wss.on("connection", onClientConnection);
+}
+
+function onClientConnection(ws) {
     console.log("Новый клиент подключился!");
+    ws.on("message", (message) => onClientMessage(ws, message));
+    ws.on("close", onClientClose);
+}
 
-    ws.on("message", (message) => {
-        console.log("Получено сообщение от клиента: " + message);
-        console.log("Отправка эхо-ответа");
-        ws.send(message);
-    });
+function onClientMessage(ws, message) {
+    console.log("Получено сообщение от клиента: " + message);
+    console.log("Отправка эхо-ответа");
+    ws.send(message);
+}
 
-    ws.on("close", () => {
-        console.log("Клиент отключился");
-    });
-});
+function onClientClose() {
+    console.log("Клиент отключился");
+}
